062fac886739842e8dfe1e45461a82ea49a83742
Michael Ilyin <milyin@gmail.com>
2025-08-17 23:50:33 +0200

    error message fix

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,15 @@ use std::path::PathBuf;
 pub fn get_project_root() -> Result<PathBuf, &'static str> {
     let project_root = env!("PROJECT_ROOT");
     if project_root.is_empty() {
-        Err(env!("PROJECT_ROOT_ERROR"))
+        let error = 
+            "The crate `prebindgen-project-root` is being used as a Cargo dependency.\n\
+            Since it is not located inside your workspace, it cannot determine the path to the workspace root.\n\
+            Please add `prebindgen-project-root` as a member of your workspace and patch dependencies to use it locally.\n\n\
+            This can be done using the helper tool:\n\n\
+            cargo install prebindgen-project-root\n\
+            cargo prebindgen-project-root install <path>\n\n\
+            where `<path>` is the path to your workspace root.\n";
+        Err(error)
     } else {
         Ok(project_root.into())
     }
